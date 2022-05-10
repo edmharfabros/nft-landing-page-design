da5f0ff36457dc2cfbddf29836a3b5daa474a57a
Edmhar <edmhrfbros@gmail.com>
2022-05-10 19:27:47 +0800

    fix hierarchy of html elements

--- pages/index.js
+++ pages/index.js
@@ -51,11 +51,8 @@ const HomePage = () => {
         <link rel="preload" as="font" href="/fonts/Inter-Medium.ttf" crossOrigin="anonymous" />
         <link rel="preload" as="font" href="/fonts/Inter-Bold.ttf" crossOrigin="anonymous" />
         <link rel="preload" as="font" href="/fonts/Inter-SemiBold.ttf" crossOrigin="anonymous" />
-
       </Head>
-
       <div className="font-inter relative">
-
         <nav className="desktop:px-[5rem] desktop:flex px-[2rem] pt-[2rem] flex items-center justify-between">
           <div className="h-[3rem]">
             <Image alt="nft landing page logo" src={logo} />
@@ -76,31 +73,31 @@ const HomePage = () => {
         <div className="animate-onload-section desktop:block desktop:right-[16%] hidden absolute top-[13.375rem] right-0 h-[32.4375rem] w-[20.5rem]">
           <img alt="robot with 2 eyes" src={`/images/hero-img-desktop.png`} />
         </div>
-        <div className="animate-onload-title desktop:pl-[5rem] desktop:pr-[50%] desktop:pb-[12.5rem] desktop:pt-[11.375rem] relative px-[2rem] pt-[5rem] pr-[30%] pb-[5rem] desktop:min-h-[calc(100vh_-_5rem)]">
-          <p className="desktop:text-[1.25rem] desktop:pb-[1rem] pb-[1.5rem] font-inter-bold text-pink">LAUNCHING SOON</p>
+        <section className="animate-onload-title desktop:pl-[5rem] desktop:pr-[50%] desktop:pb-[12.5rem] desktop:pt-[11.375rem] relative px-[2rem] pt-[5rem] pr-[30%] pb-[5rem] desktop:min-h-[calc(100vh_-_5rem)]">
+          <h2 className="desktop:text-[1.25rem] desktop:pb-[1rem] pb-[1.5rem] font-inter-bold text-pink">LAUNCHING SOON</h2>
           <h1 class="desktop:text-[6rem] desktop:pb-[2rem] desktop:leading-[7.25rem] text-[3rem] font-black pb-[1.5rem] font-inter-black leading-[3.625rem]">An NFT like no other</h1>
-          <p className="desktop:pb-[2.5625rem] leading-[1.5rem] pb-[3rem]">Don’t miss out on the release of our new NFT. Sign up below to receive updates when we go live.</p>
+          <h3 className="desktop:pb-[2.5625rem] leading-[1.5rem] pb-[3rem]">Don’t miss out on the release of our new NFT. Sign up below to receive updates when we go live.</h3>
           <button className="font-inter-semi-bold px-[3.5rem] py-[1.4375rem] bg-pink rounded-full text-white">Sign Up</button>
-        </div>
-        <div data-animation="animate-onload-section" className="section desktop:mx-[5rem] desktop:rounded-[1rem] desktop:px-[4rem] desktop:pt-[9.625rem] desktop:min-h-[20rem] px-[2rem] min-h-[20.375rem] pt-[7.625rem] text-black bg-gradient-to-r from-pink via-red via-orange to-neon-green">
-          <h1 className="desktop:mb-[1.5rem] font-inter-medium text-[2.5rem] leading-[3rem] mb-[1rem]">Free NFT for early birds</h1>
-          <p className="leading-[1.5rem] text-[1.25rem]">Sign up today and you’ll get a free NFT when we launch.</p>
-        </div>
-        <div data-animation="animate-onload-title" className="section desktop:mx-[5rem] desktop:py-[7.5rem] desktop:grid-cols-4 pt-[3.5rem] pb-[3.5rem] px-[2rem] grid grid-cols-2 auto-cols-fr grid-flow-column gap-[1.6875rem]">
+        </section>
+        <section data-animation="animate-onload-section" className="section desktop:mx-[5rem] desktop:rounded-[1rem] desktop:px-[4rem] desktop:pt-[9.625rem] desktop:min-h-[20rem] px-[2rem] min-h-[20.375rem] pt-[7.625rem] text-black bg-gradient-to-r from-pink via-red via-orange to-neon-green">
+          <h2 className="desktop:mb-[1.5rem] font-inter-medium text-[2.5rem] leading-[3rem] mb-[1rem]">Free NFT for early birds</h2>
+          <h3 className="leading-[1.5rem] text-[1.25rem]">Sign up today and you’ll get a free NFT when we launch.</h3>
+        </section>
+        <section data-animation="animate-onload-title" className="section desktop:mx-[5rem] desktop:py-[7.5rem] desktop:grid-cols-4 pt-[3.5rem] pb-[3.5rem] px-[2rem] grid grid-cols-2 auto-cols-fr grid-flow-column gap-[1.6875rem]">
           <Image alt="Boom company logo" src={companyBoom} />
           <Image alt="QRCO company logo" src={companyQrco} />
           <Image alt="Blimp company logo" src={companyBlimp} />
           <Image alt="Droplet company logo" src={companyDroplet} />
-        </div>
-        <div data-animation="animate-onload-section" className="section  desktop:mx-[5rem] mx-[2rem]">
-          <h1 className="desktop:mb-[1.5rem] font-inter-medium text-[2.5rem] leading-[3rem] mb-[1rem] ">
+        </section>
+        <section data-animation="animate-onload-section" className="section  desktop:mx-[5rem] mx-[2rem]">
+          <h2 className="desktop:mb-[1.5rem] font-inter-medium text-[2.5rem] leading-[3rem] mb-[1rem] ">
             LE Super Rare Auction
-          </h1>
-          <p className="leading-[1.5rem] text-[1.25rem]">
+          </h2>
+          <h3 className="leading-[1.5rem] text-[1.25rem]">
             We have released four limited edition NFTs early which can be bid on via OpenSea.
-          </p>
-        </div>
-        <div data-animation="animate-onload-title" className="section desktop:pl-[5rem] desktop:pt-[3.5rem] desktop:pb-[5rem] pl-[2rem] pt-[2.5rem] pb-[5.75rem] overflow-x-auto">
+          </h3>
+        </section>
+        <section data-animation="animate-onload-title" className="section desktop:pl-[5rem] desktop:pt-[3.5rem] desktop:pb-[5rem] pl-[2rem] pt-[2.5rem] pb-[5.75rem] overflow-x-auto">
           <div className="grid grid-flow-col auto-cols-[minmax(18.125rem,1fr)] grid-cols-[repeat(auto-fill,minmax(18.125rem,1fr))] gap-[1.6875rem]">
             <div className="">
               <div className="relative h-[17.375rem] w-[100%] ">
@@ -112,8 +109,8 @@ const HomePage = () => {
                   <span className="text-dark-400 font-inter-medium text-[0.75rem] leading-[1rem]">TOP BID</span>
                 </div>
                 <div className="text-dark-400 flex items-center justify-between mb-[0.75rem]">
-                  <h3 className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">Purple Man</h3>
-                  <h3 className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">2.99 ETH</h3>
+                  <p className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">Purple Man</p>
+                  <p className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">2.99 ETH</p>
                 </div>
                 <div className="flex items-center justify-between mb-[0.75rem]">
                   <p className="text-dark-400 font-inter text-[1.125rem] leading-[1rem]">#12983</p>
@@ -131,8 +128,8 @@ const HomePage = () => {
                   <span className="text-dark-400 font-inter-medium text-[0.75rem] leading-[1rem]">TOP BID</span>
                 </div>
                 <div className="text-dark-400 flex items-center justify-between mb-[0.75rem]">
-                  <h3 className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">Purple Man</h3>
-                  <h3 className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">2.99 ETH</h3>
+                  <p className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">Purple Man</p>
+                  <p className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">2.99 ETH</p>
                 </div>
                 <div className="flex items-center justify-between mb-[0.75rem]">
                   <p className="text-dark-400 font-inter text-[1.125rem] leading-[1rem]">#12983</p>
@@ -150,8 +147,8 @@ const HomePage = () => {
                   <span className="text-dark-400 font-inter-medium text-[0.75rem] leading-[1rem]">TOP BID</span>
                 </div>
                 <div className="text-dark-400 flex items-center justify-between mb-[0.75rem]">
-                  <h3 className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">Purple Man</h3>
-                  <h3 className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">2.99 ETH</h3>
+                  <p className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">Purple Man</p>
+                  <p className="text-black font-inter-medium text-[1.25rem] leading-[1rem]">2.99 ETH</p>
                 </div>
                 <div className="flex items-center justify-between mb-[0.75rem]">
                   <p className="text-dark-400 font-inter text-[1.125rem] leading-[1rem]">#12983</p>
@@ -160,19 +157,19 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="desktop:mx-[5rem] desktop:pt-[5rem] relative border-y-2 border-[#EEEEEE] pb-[5rem]">
+        </section>
+        <section className="desktop:mx-[5rem] desktop:pt-[5rem] relative border-y-2 border-[#EEEEEE] pb-[5rem]">
           <div data-animation="animate-onload-section" className="section desktop:right-[20%] absolute bottom-0 right-0 h-[29.1875rem]">
             <Image alt="girl with green shirt" src={girlWithGreenShirt} />
           </div>
           <div data-animation="animate-onload-title" className="section desktop: desktop:mx-0 desktop:max-w-[50%] relative mx-[2rem] max-w-[50%] pt-[5.75rem]">
-            <p className="pb-[1.5rem] font-inter-bold text-pink">LAUNCHING SOON</p>
+            <h2 className="pb-[1.5rem] font-inter-bold text-pink">LAUNCHING SOON</h2>
             <h1 class="desktop:text-[3.5rem] text-[3rem] font-black pb-[1.5rem] font-inter-black leading-[3.625rem]">An NFT like no other</h1>
-            <p className="desktop:text-[1.25rem] leading-[1.5rem] pb-[3rem]">Don’t miss out on the release of our new NFT. Sign up below to receive updates when we go live.</p>
+            <h3 className="desktop:text-[1.25rem] leading-[1.5rem] pb-[3rem]">Don’t miss out on the release of our new NFT. Sign up below to receive updates when we go live.</h3>
             <button className="font-inter-semi-bold px-[3.5rem] py-[1.4375rem] bg-pink rounded-full text-white">Sign Up</button>
           </div>
-        </div>
-        <div data-animation="animate-onload-title" className="section desktop:flex desktop:justify-between desktop:mx-[5rem] pt-[4rem] mx-[2rem]">
+        </section>
+        <section data-animation="animate-onload-title" className="section desktop:flex desktop:justify-between desktop:mx-[5rem] pt-[4rem] mx-[2rem]">
           <div className="desktop:w-[40%] w-full">
             <div className="h-[3rem]">
               <Image alt="nft landing page logo" src={logo} />
@@ -217,7 +214,7 @@ const HomePage = () => {
               <p className="font-inter text-[1.25rem] leading-[1rem] text-dark mb-[1.25rem]">Instagram</p>
             </div>
           </div>
-        </div>
+        </section>
         <p data-animation="animate-onload-title" className="section desktop:mx-[5rem] desktop:mt-[5rem] mx-[2rem] mt-[4rem] mb-[6.5625rem] font-inter text-[1.25rem] leading-[1rem] text-dark-200">© Copyright 2022 NFTlanding</p>
       </div>
     </>
